78c78c9b32b4a24dee96f0a3ad5b55915c9bfbaa
CadenFinley <147097345+CadenFinley@users.noreply.github.com>
2025-03-27 00:59:01 -0500

    Revert "Create ocean.sh"
    This reverts commit 5d0b6bcb94d2f1e0c1e6b6114e23cf09b8e047bc.

--- themes/ocean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-# Ocean theme colors
-DEVTOOLSZSH_SHELL_COLOR="\033[1;36m"      # Light Cyan
-DEVTOOLSZSH_DIRECTORY_COLOR="\033[1;34m"  # Blue
-DEVTOOLSZSH_BRANCH_COLOR="\033[1;96m"     # Bright Cyan
-DEVTOOLSZSH_GIT_COLOR="\033[1;35m"        # Purple
