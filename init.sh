a600c9dc6fcdc663df56dd8521be275e53ad01de
CadenFinley <147097345+CadenFinley@users.noreply.github.com>
2025-03-27 00:40:25 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,7 +4,10 @@
 if [[ -z "$DEVTOOLSZSH_INITIALIZED" ]]; then
     export DEVTOOLSZSH_INITIALIZED=true
     export DEVTOOLSZSH_DISPLAY_WHOLE_PATH=${DEVTOOLSZSH_DISPLAY_WHOLE_PATH:-false}
-    export DEVTOOLSZSH_THEME=${DEVTOOLSZSH_THEME:-default}
+    # Only set DEVTOOLSZSH_THEME if it is not already set
+    if [[ -z "$DEVTOOLSZSH_THEME" ]]; then
+        export DEVTOOLSZSH_THEME="default"
+    fi
     export DEVTOOLSZSH_AUTO_UPDATE=${DEVTOOLSZSH_AUTO_UPDATE:-false}
     
     # Get base directory
